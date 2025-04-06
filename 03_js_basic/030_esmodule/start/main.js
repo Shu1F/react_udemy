4973ba16ad95e7f90c066a79e0c5b3ad23a6dc3c
fs203 <fs2031@icloud.com>
2025-04-07 08:34:06 +0900

    update

---
+++ 03_js_basic/030_esmodule/start/main.js
@@ -0,0 +1,8 @@
+import { hello } from "./module.js";
+import { User } from "./module.js";
+import functionB from "./module.js";
+
+functionB();
+hello();
+const user = new User("Tom");
+user.hello();
