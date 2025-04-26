badd704175ce7f38eab60dee1001151cb8852176
fs203 <fs2031@icloud.com>
2025-04-26 15:05:55 +0900

    finished : event type

--- 05_state_and_event/src/010_eventlistener/start/Example.jsx
+++ 05_state_and_event/src/010_eventlistener/start/Example.jsx
@@ -1,6 +1,15 @@
 const Example = () => {
+  const clickHundler = () => {
+    alert("ボタンがクリックされました");
+  };
+  const clickHundler2 = () => {
+    console.log("ボタンがクリックされました");
+  };
   return (
-    <></>
+    <>
+      <button onClick={clickHundler}>クリックしてね</button>
+      <button onClick={clickHundler2}>クリックしてね</button>
+    </>
   );
 };
 
