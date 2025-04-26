74ddf65255ed1d08b50866134dce7136eb48f4a8
fs203 <fs2031@icloud.com>
2025-04-26 16:41:36 +0900

    finished : practice_useState

--- 05_state_and_event/src/040_multiple_state/start/Example.jsx
+++ 05_state_and_event/src/040_multiple_state/start/Example.jsx
@@ -1,5 +1,36 @@
+import { useState } from "react";
 const Example = () => {
-  
+  let [btnCount, setBtnCount] = useState(0);
+  let [btnCount2, setBtnCount2] = useState(10);
+  let [btnCount3, setBtnCount3] = useState(100);
+  return (
+    <>
+      <button
+        onClick={() => {
+          setBtnCount(btnCount + 1);
+        }}
+      >
+        ボタンA
+      </button>
+      = ボタンを{btnCount}回押しました！
+      <button
+        onClick={() => {
+          setBtnCount2(btnCount2 + 1);
+        }}
+      >
+        ボタンB
+      </button>
+      = ボタンを{btnCount2}回押しました！
+      <button
+        onClick={() => {
+          setBtnCount3(btnCount3 + 1);
+        }}
+      >
+        ボタンC
+      </button>
+      = ボタンを{btnCount3}回押しました！
+    </>
+  );
 };
 
 export default Example;
--- 05_state_and_event/src/055_practice_state/start/Example.jsx
+++ 05_state_and_event/src/055_practice_state/start/Example.jsx
@@ -1,13 +1,18 @@
+import { useState } from "react";
+
 const Example = () => {
+  let [counter, setCounter] = useState(0);
+  let countUp = () => setCounter(counter + 1);
+  let countDown = () => setCounter(counter - 1);
   return (
     <>
       <h3>練習問題</h3>
       <p>
         記述を変更し、完成コードのように+と-ボタンをクリックすると現在のカウント数が1ずつ増減する機能を実装してください。*useStateを用いてcountとsetCountを定義してください。
       </p>
-      <p>現在のカウント数: {/* ここにcountを表示してください。*/}</p>
-      <button>+</button>
-      <button>-</button>
+      <p>現在のカウント数: {counter}</p>
+      <button onClick={countUp}>+</button>
+      <button onClick={countDown}>-</button>
     </>
   );
 };
