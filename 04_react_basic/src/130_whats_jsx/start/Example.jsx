2af6a6a3c4321f1dcaaf036c600942a0ae31a64c
fs203 <fs2031@icloud.com>
2025-05-01 15:46:33 +0900

    finished: array

--- 04_react_basic/src/130_whats_jsx/start/Example.jsx
+++ 04_react_basic/src/130_whats_jsx/start/Example.jsx
@@ -2,10 +2,7 @@ import React from "react";
 
 const Example = () => {
   const sample1 = <h1 className="greeting">Hello World</h1>;
-
-  // React.createElement("h1", {
-  //   className: "greeting"
-  // }, "Hello World");
+  console.log(sample1);
 
   const sample2 = (
     <div>
@@ -14,14 +11,14 @@ const Example = () => {
     </div>
   );
 
-  console.log(
-    (
-      <div>
-        <h1>Hello!</h1>
-        <h2>Good to see you.</h2>
-      </div>
-    ).props
-  );
+  // console.log(
+  //   (
+  //     <div>
+  //       <h1>Hello!</h1>
+  //       <h2>Good to see you.</h2>
+  //     </div>
+  //   ).props
+  // );
 
   return React.createElement(
     "div",
--- 06_control_and_form/src/130_reminder/start/Example.jsx
+++ 06_control_and_form/src/130_reminder/start/Example.jsx
@@ -1,8 +1,11 @@
+import Todo from "./components/Todo";
+
 const Example = () => {
+  // const [remindVal, setRemindVal] = useState("");
   return (
     <>
-      <h3>完成系と同じような機能を作成してください。</h3>
-      <p>また、枠線で囲まれた部品をコンポーネントに分離してください。</p>
+      <h2>Reminder</h2>
+      <Todo />
     </>
   );
 };
